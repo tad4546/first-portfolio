57fb52f4669a7139c792cfc0d462961805652f7b
tad4546 <61728827+tad4546@users.noreply.github.com>
2023-02-17 08:22:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function light(){
+    document.body.style.background="rgb(51,51,51)"
+    document.querySelector('h1').style.color="white"
+    document.getElementById('hd').style.color="white"
+    document.getElementById('hd2').style.color="white"
+    document.querySelector('p').style.color="white"
+}
+//these functions are vise versa, if you click on the sun, you are actually turning it off
+//and when you click on the moon, you are actually turning on the sun
+function dark(){
+    document.body.style.background="rgb(221, 219, 219)";
+    document.querySelector('h1').style.color="black"
+    document.getElementById('hd').style.color="black"
+    document.getElementById('hd2').style.color="black"
+    document.querySelector('p').style.color="black"
+    //document.getElementById('light').style.color="rgb(206,206,0)"
+    
+    
+}
+
+
+
+
+
