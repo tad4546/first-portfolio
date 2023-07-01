e0fe0cfaf60072298a9dd282eea8440b85bd4647
tad4546 <61728827+tad4546@users.noreply.github.com>
2023-07-01 11:01:38 +0200

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function light(){
+    document.body.style.transition = "background-color 0.5s, color 0.5s";
     document.body.style.background="rgb(51,51,51)"
     document.querySelector('h1').style.color="white"
     document.getElementById('hd').style.color="white"
@@ -8,6 +9,7 @@ function light(){
 //these functions are vise versa, if you click on the sun, you are actually turning it off
 //and when you click on the moon, you are actually turning on the sun
 function dark(){
+    document.body.style.transition = "background-color 0.5s, color 0.5s";
     document.body.style.background="rgb(221, 219, 219)";
     document.querySelector('h1').style.color="black"
     document.getElementById('hd').style.color="black"
